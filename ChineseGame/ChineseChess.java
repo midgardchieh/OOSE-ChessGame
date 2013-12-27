a70eb65e9610ec8828a9a8b2c9767ae044f3ebe8
Midgardchieh <midgardchieh@gmail.com>
2013-12-27 18:15:30 +0800

    v5.0
    加上保存棋譜及演示棋譜

--- ChineseGame/ChineseChess.java
+++ ChineseGame/ChineseChess.java
@@ -12,6 +12,7 @@ import java.util.LinkedList;
  */
 public class ChineseChess extends JFrame implements ActionListener {
 	ChessBoard board = null;
+	Demon demon = null;
 	MakeChessManual record = null;
 	Container con = null;
 	JMenuBar bar;
@@ -25,10 +26,17 @@ public class ChineseChess extends JFrame implements ActionListener {
 		fileMenu = new JMenu("!!!!!!!!!!!按我製作棋譜!!!!!!!!!!");
 		setTitle("中國象棋");
 		makeManual = new JMenuItem("製作棋譜");
+		saveManual = new JMenuItem("保存棋譜");
+		saveManual.setEnabled(false);
+		playManual = new JMenuItem("演示棋譜");
 		fileMenu.add(makeManual);
+		fileMenu.add(saveManual);
+		fileMenu.add(playManual);
 		bar.add(fileMenu);
 		setJMenuBar(bar);
 		makeManual.addActionListener(this);
+		saveManual.addActionListener(this);
+		playManual.addActionListener(this);
 		board = new ChessBoard(45, 45, 9, 10);
 		
 		record = board.record;
@@ -54,6 +62,7 @@ public class ChineseChess extends JFrame implements ActionListener {
 	public void actionPerformed(ActionEvent e) {
 		if (e.getSource() == makeManual) {
 			con.removeAll();
+			saveManual.setEnabled(true);
 			this.setTitle(makeManual.getText());
 			board = new ChessBoard(45, 45, 9, 10);
 			record = board.record;
@@ -63,6 +72,64 @@ public class ChineseChess extends JFrame implements ActionListener {
 			split.setDividerLocation(460);
 			con.add(split, BorderLayout.CENTER);
 			validate();
+		}
+		if (e.getSource() == saveManual) {
+			int state = fileChooser.showSaveDialog(null);
+			File saveFile = fileChooser.getSelectedFile();
+			if (saveFile != null && state == JFileChooser.APPROVE_OPTION) {
+				try {
+					FileOutputStream outOne = new FileOutputStream(saveFile);
+					ObjectOutputStream outTwo = new ObjectOutputStream(outOne);
+					outTwo.writeObject(record.getManual());
+					outOne.close();
+					outTwo.close();
+				} catch (IOException event) {
+				}
+			}
+		}
+		if (e.getSource() == playManual) {
+			con.removeAll();
+			con.repaint();
+			con.validate();
+			validate();
+			saveManual.setEnabled(false);
+
+			int state = fileChooser.showOpenDialog(null);
+			File openFile = fileChooser.getSelectedFile();
+			if (openFile != null && state == JFileChooser.APPROVE_OPTION) {
+				try {
+					FileInputStream inOne = new FileInputStream(openFile);
+					ObjectInputStream inTwo = new ObjectInputStream(inOne);
+					Manual = (LinkedList) inTwo.readObject();
+					inOne.close();
+					inTwo.close();
+					ChessBoard board = new ChessBoard(45, 45, 9, 10);
+					demon = new Demon(board);
+					demon.setManual(Manual);
+					con.add(demon, BorderLayout.CENTER);
+					con.validate();
+					validate();
+					this.setTitle(playManual.getText() + ":" + openFile);
+				} catch (Exception event) {
+					JLabel label = new JLabel("不是棋譜文件");
+					label.setFont(new Font("標楷體", Font.BOLD, 60));
+					label.setForeground(Color.red);
+					label.setHorizontalAlignment(SwingConstants.CENTER);
+					con.add(label, BorderLayout.CENTER);
+					con.validate();
+					this.setTitle("沒有打開棋譜");
+					validate();
+				}
+			} else {
+				JLabel label = new JLabel("沒有打開棋譜文件");
+				label.setFont(new Font("標楷體", Font.BOLD, 50));
+				label.setForeground(Color.pink);
+				label.setHorizontalAlignment(SwingConstants.CENTER);
+				con.add(label, BorderLayout.CENTER);
+				con.validate();
+				this.setTitle("沒有打開棋譜文件");
+				validate();
+			}
 		}
 	}
 
---
+++ ChineseGame/Demon.java
@@ -0,0 +1,147 @@
+﻿package ChineseChess;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.*;
+import java.util.*;
+
+/**
+ * 演示棋譜
+ */
+public class Demon extends JPanel implements ActionListener {
+	public JButton replay = null, next = null;
+	LinkedList Manual = null;
+	int index = -1;
+	ChessBoard board = null;
+	JTextArea text;
+	JSplitPane splitH = null, splitV = null;
+
+	public Demon(ChessBoard board) {
+		this.board = board;
+		replay = new JButton("重新演示");
+		next = new JButton("下一步");
+		replay.addActionListener(this);
+		next.addActionListener(this);
+		text = new JTextArea();
+		setLayout(new BorderLayout());
+		JScrollPane pane = new JScrollPane(text);
+		JPanel p = new JPanel(new GridLayout(3, 2));
+		p.add(next);
+		p.add(replay);
+		splitV = new JSplitPane(JSplitPane.VERTICAL_SPLIT, pane, p);
+		splitH = new JSplitPane(JSplitPane.HORIZONTAL_SPLIT, board, splitV);
+		splitV.setDividerSize(5);
+		splitV.setDividerLocation(400);
+		splitH.setDividerSize(5);
+		splitH.setDividerLocation(460);
+		add(splitH, BorderLayout.CENTER);
+		validate();
+	}
+
+	public void setManual(LinkedList Manual) {
+		this.Manual = Manual;
+	}
+
+	public char numberToLetter(int n) {
+		char c = '\0';
+		switch (n) {
+		case 1:
+			c = 'A';
+			break;
+		case 2:
+			c = 'B';
+			break;
+		case 3:
+			c = 'C';
+			break;
+		case 4:
+			c = 'D';
+			break;
+		case 5:
+			c = 'E';
+			break;
+		case 6:
+			c = 'F';
+			break;
+		case 7:
+			c = 'G';
+			break;
+		case 8:
+			c = 'H';
+			break;
+		case 9:
+			c = 'I';
+			break;
+		case 10:
+			c = 'J';
+			break;
+		}
+		return c;
+	}
+
+	public void actionPerformed(ActionEvent e) {
+		if (e.getSource() == next) {
+			index++;
+			if (index < Manual.size()) {
+				playonestep(index);
+			} else {
+				playover("棋譜演示完畢");
+			}
+		}
+		if (e.getSource() == replay) {
+			board = new ChessBoard(45, 45, 9, 10);
+			splitH.remove(board);
+			splitH.setDividerSize(5);
+			splitH.setDividerLocation(460);
+			splitH.setLeftComponent(board);
+			splitH.validate();
+			index = -1;
+			text.setText(null);
+		}
+
+	}
+
+	
+
+	public void playonestep(int index) {
+		MoveStep step = (MoveStep) Manual.get(index);
+		Point pStart = step.pStart;
+		Point pEnd = step.pEnd;
+		int startI = pStart.x;
+		int startJ = pStart.y;
+		int endI = pEnd.x;
+		int endJ = pEnd.y;
+		ChessPiece piece = (board.point)[startI][startJ].getPiece();
+		if ((board.point)[endI][endJ].isPiece() == true) {
+			ChessPiece pieceRemoved = (board.point)[endI][endJ].getPiece();
+			(board.point)[endI][endJ].reMovePiece(pieceRemoved, board);
+			board.repaint();
+			(board.point)[endI][endJ].setPiece(piece, board);
+			(board.point)[startI][startJ].sethaveChess(false);
+			board.repaint();
+		} else {
+			(board.point)[endI][endJ].setPiece(piece, board);
+			(board.point)[startI][startJ].sethaveChess(false);
+
+		}
+		String Chesstype = piece.Chesstype();
+		String name = piece.getName();
+		String m = "#" + Chesstype + name + ": " + startI + numberToLetter(startJ)
+				+ " 到 " + endI + numberToLetter(endJ);
+		text.append(m);
+		if (piece.Chesstype().equals(board.blackcolor))
+			text.append("\n");
+	}
+
+	public void playover(String message) {
+		splitH.remove(board);
+		splitH.setDividerSize(5);
+		splitH.setDividerLocation(460);
+		JLabel label = new JLabel(message);
+		label.setFont(new Font("標楷體", Font.BOLD, 40));
+		label.setForeground(Color.blue);
+		label.setHorizontalAlignment(SwingConstants.CENTER);
+		splitH.setLeftComponent(label);
+		splitH.validate();
+	}
+}
