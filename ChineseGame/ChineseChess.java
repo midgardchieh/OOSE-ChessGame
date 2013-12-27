355f8e3ddfd4006cf9f59732d4a2803ad3fd1f7c
Midgardchieh <midgardchieh@gmail.com>
2013-12-27 18:14:32 +0800

    v4.0
    加上選單
    可以重新一局

--- ChineseGame/ChineseChess.java
+++ ChineseGame/ChineseChess.java
@@ -8,17 +8,27 @@ import java.io.*;
 import java.util.LinkedList;
 
 /**
- * 象棋主類別 
+ * 象棋主類別
  */
 public class ChineseChess extends JFrame implements ActionListener {
 	ChessBoard board = null;
 	MakeChessManual record = null;
 	Container con = null;
+	JMenuBar bar;
+	JMenu fileMenu;
+	JMenuItem makeManual, saveManual, playManual;
 	JFileChooser fileChooser = null;
 	LinkedList Manual = null;
 
-	public ChineseChess() {
+	public ChineseChess() {		
+		bar = new JMenuBar();
+		fileMenu = new JMenu("!!!!!!!!!!!按我製作棋譜!!!!!!!!!!");
 		setTitle("中國象棋");
+		makeManual = new JMenuItem("製作棋譜");
+		fileMenu.add(makeManual);
+		bar.add(fileMenu);
+		setJMenuBar(bar);
+		makeManual.addActionListener(this);
 		board = new ChessBoard(45, 45, 9, 10);
 		
 		record = board.record;
@@ -42,6 +52,18 @@ public class ChineseChess extends JFrame implements ActionListener {
 	}
 
 	public void actionPerformed(ActionEvent e) {
+		if (e.getSource() == makeManual) {
+			con.removeAll();
+			this.setTitle(makeManual.getText());
+			board = new ChessBoard(45, 45, 9, 10);
+			record = board.record;
+			JSplitPane split = new JSplitPane(JSplitPane.HORIZONTAL_SPLIT,
+					true, board, record);
+			split.setDividerSize(5);
+			split.setDividerLocation(460);
+			con.add(split, BorderLayout.CENTER);
+			validate();
+		}
 	}
 
 	public static void main(String args[]) {
